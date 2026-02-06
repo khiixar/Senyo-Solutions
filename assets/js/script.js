8aa42ca84c056ccb36a45c93dc439db9cd67ab5d
khiixar <khiixar@users.noreply.github.com>
2026-02-05 22:05:42 -0500

    changes fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -808,6 +808,7 @@ function initTiltEffects() {
 }
 
 // Navbar scroll effect
+window.addEventListener('scroll', function() {
     const navbar = document.querySelector('.navbar');
     if (window.scrollY > 50) {
         navbar.classList.add('scrolled');
